33ebb38610cc11899be1665a42aca7a6c8e09428
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-22 18:36:06 +0200

    Refactor

--- examples/download_game_images.rs
+++ examples/download_game_images.rs
@@ -7,23 +7,24 @@ use igdb_client::media_quality::MediaQuality;
 fn main() {
     task::block_on(async {
 
-        let igdb_client = IGDBClient::new("586677e082e930d4c44a59962420e9d1");
+        let igdb_client = IGDBClient::new("user-key");
         let games_client = igdb_client.games();
         let witcher = games_client.get_by_name("Witcher 3").await.unwrap();
 
         let covers_client = igdb_client.covers();
-
         let covers_response = covers_client.get_by_game_id(witcher.id).await.unwrap();
-        let first_cover = covers_response.first().unwrap();
+
+        let cover = covers_response.first().unwrap();
 
         let screenshots_client = igdb_client.screenshots();
         let screenshots_response = screenshots_client.get_by_game_id(witcher.id).await.unwrap();
-        let first_screenshot = screenshots_response.first().unwrap();
 
-        covers_client.download_by_id(first_cover.id.to_string(),
+        let screenshot = screenshots_response.first().unwrap();
+
+        covers_client.download_by_id(cover.id.to_string(),
                                      "cover.jpg", MediaQuality::ScreenshotHuge).await.unwrap();
 
-        screenshots_client.download_by_id(first_screenshot.id.to_string(),
+        screenshots_client.download_by_id(screenshot.id.to_string(),
                                           "screenshot.jpg", MediaQuality::ScreenshotHuge).await.unwrap();
     })
 }
