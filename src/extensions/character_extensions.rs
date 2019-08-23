9af99411258a2e1aad04e757b05ab143cb24cfd4
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-23 17:00:39 +0200

    Add docs

--- src/extensions/character_extensions.rs
+++ src/extensions/character_extensions.rs
@@ -4,6 +4,16 @@ use crate::request_builder::RequestBuilder;
 
 impl CharacterClient {
     ///Retrieves the characters for a given game id
+    ///
+    /// # Examples
+    /// ```
+    /// use igdb_client::client::IGDBClient;
+    ///
+    /// let igdb_client = IGDBClient::new("api-key");
+    /// let characters_client = igdb_client.characters();
+    /// ///Retrieve 8 first characters for The Witcher 3 game
+    /// let characters = characters_client.get_by_game_id(1942, 8);
+    /// ```
     pub async fn get_by_game_id(
         &self,
         game_id: usize,
