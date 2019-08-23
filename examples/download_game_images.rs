4250d4450c65156a32096bc3a8308c43b42fa5df
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-23 10:53:24 +0200

    cargo fmt

--- examples/download_game_images.rs
+++ examples/download_game_images.rs
@@ -14,10 +14,12 @@ fn main() {
 
         //Get the first 3 screenshots for the Witcher 3 game
         let screenshots_client = igdb_client.screenshots();
-        let screenshots_response = screenshots_client.get_by_game_id(witcher.id, 3).await.unwrap();
+        let screenshots_response = screenshots_client
+            .get_by_game_id(witcher.id, 3)
+            .await
+            .unwrap();
 
         for (i, cover) in covers_response.iter().enumerate() {
-
             covers_client
                 .download_by_id(
                     cover.id.to_string(),
@@ -29,15 +31,14 @@ fn main() {
         }
 
         for (i, screenshot) in screenshots_response.iter().enumerate() {
-
-        screenshots_client
-            .download_by_id(
-                screenshot.id.to_string(),
-                format!("screenshot{}.jpg", i),
-                MediaQuality::ScreenshotHuge,
-            )
-            .await
-            .unwrap();
-         }
+            screenshots_client
+                .download_by_id(
+                    screenshot.id.to_string(),
+                    format!("screenshot{}.jpg", i),
+                    MediaQuality::ScreenshotHuge,
+                )
+                .await
+                .unwrap();
+        }
     })
 }
