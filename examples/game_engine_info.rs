9d71d2b1cd1679abec6a482e9656a50214a4e33c
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-23 09:46:25 +0200

    Rename sample and update version

--- examples/game_engine_info.rs
+++ examples/game_engine_info.rs
@@ -6,13 +6,10 @@ fn main() {
         let igdb_client = IGDBClient::new("user-key");
 
         let games_client = igdb_client.games();
-
         let game = games_client.get_by_name("Riders of Asgard").await.unwrap();
-
         let engine_id = game.game_engines.first().unwrap();
 
         let engines_client = igdb_client.game_engines();
-        println!("{}", game.game_engines.len());
         let engine = engines_client.get_by_id(*engine_id as usize).await.unwrap();
 
         println!("{:?}", engine);
